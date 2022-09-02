1db3a62d3eb7b8e0ff228090cca8dcf8e7e6adc9
Vladislav Kantaev <vvkantaev@yandex.ru>
2022-09-02 18:21:16 +0300

    Initialize DirectX 12

---
+++ DX12Renderer/Helpers.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#define WIN32_LEAN_AND_MEAN
+#include <exception>
+#include <Windows.h> // For HRESULT
+
+// From DXSampleHelper.h 
+// Source: https://github.com/Microsoft/DirectX-Graphics-Samples
+inline void ThrowIfFailed(const HRESULT result)
+{
+    if (FAILED(result))
+    {
+        throw std::exception();
+    }
+}
+
+inline void ThrowIfNegative(const int value)
+{
+	if (value < 0)
+	{
+		throw std::exception();
+	}
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# DX12 Renderer
+
+## Sources:
+- https://www.3dgep.com/learning-directx-12-1/
