103d6c5725c78380f8ac15b19c83332ea4cfccae
Vladislav Kantaev <vvkantaev@yandex.ru>
2022-09-18 18:57:48 +0300

    Implement structured buffer resource

---
+++ DX12Renderer/StructuredBuffer.cpp
@@ -0,0 +1,82 @@
+#include "StructuredBuffer.h"
+#include "d3dx12.h"
+#include "Application.h"
+#include "Buffer.h"
+
+StructuredBuffer::StructuredBuffer(const std::wstring& name /*= L""*/)
+	: Buffer(name)
+	, m_CounterBuffer(CD3DX12_RESOURCE_DESC::Buffer(4, D3D12_RESOURCE_FLAG_ALLOW_UNORDERED_ACCESS), 1, 4, name + L" Counter")
+	, m_NumElements(0)
+	, m_ElementSize(0)
+{
+	m_Srv = Application::Get().AllocateDescriptors(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV);
+	m_Uav = Application::Get().AllocateDescriptors(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV);
+}
+
+StructuredBuffer::StructuredBuffer(const D3D12_RESOURCE_DESC resourceDesc, size_t numElements, size_t elementSize, const std::wstring& name /*= L""*/)
+	: Buffer(resourceDesc, numElements, elementSize, name)
+	, m_CounterBuffer(CD3DX12_RESOURCE_DESC::Buffer(4, D3D12_RESOURCE_FLAG_ALLOW_UNORDERED_ACCESS), 1, 4, name + L" Counter")
+	, m_NumElements(numElements)
+	, m_ElementSize(elementSize)
+{
+	m_Srv = Application::Get().AllocateDescriptors(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV);
+	m_Uav = Application::Get().AllocateDescriptors(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV);
+}
+
+size_t StructuredBuffer::GetNumElements() const
+{
+	return m_NumElements;
+}
+
+size_t StructuredBuffer::GetElementSize() const
+{
+	return m_ElementSize;
+}
+
+void StructuredBuffer::CreateViews(size_t numElements, size_t elementSize)
+{
+	auto device = Application::Get().GetDevice();
+
+	m_NumElements = numElements;
+	m_ElementSize = elementSize;
+
+	D3D12_SHADER_RESOURCE_VIEW_DESC srvDesc = {};
+	srvDesc.ViewDimension = D3D12_SRV_DIMENSION_BUFFER;
+	srvDesc.Format = DXGI_FORMAT_UNKNOWN;
+	srvDesc.Shader4ComponentMapping = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
+	srvDesc.Buffer.NumElements = static_cast<UINT>(m_NumElements);
+	srvDesc.Buffer.StructureByteStride = static_cast<UINT>(m_ElementSize);
+	srvDesc.Buffer.Flags = D3D12_BUFFER_SRV_FLAG_NONE;
+
+	device->CreateShaderResourceView(m_d3d12Resource.Get(),
+		&srvDesc,
+		m_Srv.GetDescriptorHandle());
+
+	D3D12_UNORDERED_ACCESS_VIEW_DESC uavDesc = {};
+	uavDesc.ViewDimension = D3D12_UAV_DIMENSION_BUFFER;
+	uavDesc.Format = DXGI_FORMAT_UNKNOWN;
+	uavDesc.Buffer.CounterOffsetInBytes = 0;
+	uavDesc.Buffer.NumElements = static_cast<UINT>(m_NumElements);
+	uavDesc.Buffer.StructureByteStride = static_cast<UINT>(m_ElementSize);
+	uavDesc.Buffer.Flags = D3D12_BUFFER_UAV_FLAG_NONE;
+
+	device->CreateUnorderedAccessView(m_d3d12Resource.Get(),
+		m_CounterBuffer.GetD3D12Resource().Get(),
+		&uavDesc,
+		m_Uav.GetDescriptorHandle());
+}
+
+D3D12_CPU_DESCRIPTOR_HANDLE StructuredBuffer::GetShaderResourceView(const D3D12_SHADER_RESOURCE_VIEW_DESC* srvDesc /*= nullptr*/) const
+{
+	return m_Srv.GetDescriptorHandle();
+}
+
+D3D12_CPU_DESCRIPTOR_HANDLE StructuredBuffer::GetUnorderedAccessView(const D3D12_UNORDERED_ACCESS_VIEW_DESC* uavDesc /*= nullptr*/) const
+{
+	return m_Uav.GetDescriptorHandle();
+}
+
+const ByteAddressBuffer& StructuredBuffer::GetCounterBuffer()
+{
+	return m_CounterBuffer;
+}
---
+++ DX12Renderer/StructuredBuffer.h
@@ -0,0 +1,67 @@
+#pragma once
+
+/*
+ *  Copyright(c) 2018 Jeremiah van Oosten
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files(the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions :
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ *  IN THE SOFTWARE.
+ */
+
+ /**
+  *  @file StructuredBuffer.h
+  *  @date October 24, 2018
+  *  @author Jeremiah van Oosten
+  *
+  *  @brief Structured buffer resource.
+  */
+
+#include "Buffer.h"
+#include <string>
+#include "DescriptorAllocation.h"
+#include "ByteAddressBuffer.h"
+
+class StructuredBuffer final : public Buffer 
+{
+public:
+	explicit StructuredBuffer(const std::wstring& name = L"");
+	explicit StructuredBuffer(const D3D12_RESOURCE_DESC resourceDesc,
+		size_t numElements, size_t elementSize,
+		const std::wstring& name = L"");
+
+	size_t GetNumElements() const;
+
+	size_t GetElementSize() const;
+
+	void CreateViews(size_t numElements, size_t elementSize) override;
+
+	D3D12_CPU_DESCRIPTOR_HANDLE GetShaderResourceView(const D3D12_SHADER_RESOURCE_VIEW_DESC* srvDesc = nullptr) const override;
+
+	D3D12_CPU_DESCRIPTOR_HANDLE GetUnorderedAccessView(const D3D12_UNORDERED_ACCESS_VIEW_DESC* uavDesc = nullptr) const override;
+
+	const ByteAddressBuffer& GetCounterBuffer();
+
+private:
+	size_t m_NumElements;
+	size_t m_ElementSize;
+
+	DescriptorAllocation m_Srv;
+	DescriptorAllocation m_Uav;
+
+	ByteAddressBuffer m_CounterBuffer;
+};
+
