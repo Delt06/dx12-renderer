1a585ff2774c393a1084736f4d6732da20036feb
Vladislav Kantaev <vvkantaev@yandex.ru>
2022-09-20 15:36:44 +0300

    Temp Improve SSLR

--- DX12Renderer/Shaders/SSR_Trace_PixelShader.hlsl
+++ DX12Renderer/Shaders/SSR_Trace_PixelShader.hlsl
@@ -28,12 +28,18 @@ Texture2D depth : register(t2);
 
 SamplerState defaultSampler : register(s0);
 
-float3 SamplePositionVS(float2 uv)
+float3 SamplePositionVS(float2 uv, out float out_zNDC)
 {
-    float zNDC = depth.SampleLevel(defaultSampler, uv, 0).x;
-    float3 positionNDC = ScreenSpaceUVToNDC(uv, zNDC);
+    out_zNDC = depth.SampleLevel(defaultSampler, uv, 0).x;
+    float3 positionNDC = ScreenSpaceUVToNDC(uv, out_zNDC);
     float3 positionVS = RestorePositionVS(positionNDC, InverseProjection).xyz;
     return positionVS;
+}
+
+float3 SamplePositionVS(float2 uv)
+{
+    float out_zNDC;
+    return SamplePositionVS(uv, out_zNDC);
 }
 
 float3 SampleNormalVS(float2 uv)
@@ -42,120 +48,110 @@ float3 SampleNormalVS(float2 uv)
     return normalize(mul((float3x3) View, normalWS));
 }
 
-float4 ToScreenSpace(float3 positionVS)
+float4 ToScreenSpaceUV(float3 positionVS)
 {
     float4 positionCS = mul(Projection, float4(positionVS, 1.0));
     positionCS.xyz /= positionCS.w;
     
     positionCS.xy = positionCS.xy * 0.5 + 0.5;
     positionCS.y = 1 - positionCS.y;
-    positionCS.xy /= TexelSize;
     
     return positionCS;
+}
+
+float4 SampleSceneColor(float2 uv)
+{
+    return sceneColor.Sample(defaultSampler, uv);
+}
+
+struct TraceOutput
+{
+    bool Hit;
+    float2 UV;
+};
+
+#define SAMPLES_COUNT 16
+
+static const float2 RANDOM_PIXEL_OFFSETS[SAMPLES_COUNT] =
+{
+    float2(0.94558609, -0.76890725),
+	float2(-0.81544232, -0.87912464),
+	float2(0.97484398, 0.75648379),
+	float2(-0.91588581, 0.45771432),
+	float2(-0.94201624, -0.39906216),
+	float2(-0.094184101, -0.92938870),
+	float2(0.34495938, 0.29387760),
+	float2(-0.38277543, 0.27676845),
+	float2(0.44323325, -0.97511554),
+	float2(0.53742981, -0.47373420),
+	float2(-0.26496911, -0.41893023),
+	float2(0.79197514, 0.19090188),
+	float2(-0.24188840, 0.99706507),
+	float2(-0.81409955, 0.91437590),
+	float2(0.19984126, 0.78641367),
+	float2(0.14383161, -0.14100790)
+};
+
+TraceOutput Trace(float2 uv)
+{
+    float3 originVS = SamplePositionVS(uv);
+    float initialDepth = originVS.z;
+    float3 normalVS = SampleNormalVS(uv);
+    
+    float3 viewDir = normalize(originVS);
+    float3 reflectDir = normalize(reflect(viewDir, normalVS));
+    
+    float currentLength = 1;
+    uint loops = 10;
+    
+    TraceOutput output = (TraceOutput) 0;
+    
+    for (uint i = 0; i < loops && !output.Hit; ++i)
+    {
+        float3 currentPosition = originVS + reflectDir * currentLength;
+        float4 currentUV = ToScreenSpaceUV(currentPosition);
+        
+        float gBufferDepth;
+        float3 currentGBufferPositionVS = SamplePositionVS(currentUV.xy, gBufferDepth);
+        float currentDepth = currentGBufferPositionVS.z;
+        
+        for (uint j = 0; j < SAMPLES_COUNT; ++j)
+        {
+            if (abs(currentPosition.z - currentDepth) < 0.001)
+            {
+                output.Hit = true;
+                output.UV = currentUV.xy;
+                break;
+            }
+            
+            // jitter sample depth
+            currentDepth = SamplePositionVS(currentUV.xy + RANDOM_PIXEL_OFFSETS[j] * 2 * TexelSize).z;
+        }
+        
+        // hit skybox
+        if (gBufferDepth > 0.9999)
+        {
+            break;
+        }
+        
+        float3 newPosition = currentGBufferPositionVS;
+        newPosition.z = currentDepth;
+        currentLength = length(originVS - newPosition);
+    }
+    
+    return output;
 }
 
 float4 main(PixelShaderInput IN) : SV_TARGET
 {
     float2 uv = IN.UV;
-    
-    float3 positionFrom = SamplePositionVS(uv);
-    float3 directionFrom = normalize(positionFrom);
-    float3 normal = SampleNormalVS(uv);
-    float3 reflectionDirection = reflect(directionFrom, normal);
-    float3 positionTo = positionFrom;
-    
-    float3 startVS = positionFrom;
-    float3 endVS = positionFrom + reflectionDirection * MaxDistance;
-    
-    // SS is for Screen Space
-    float4 startSS = ToScreenSpace(startVS);
-    float4 endSS = ToScreenSpace(endVS);
-    
-    
-    float2 deltaVector = endSS.xy - startSS.xy;
-    float2 absDeltaVector = abs(deltaVector);
-    float useX = absDeltaVector.x >= absDeltaVector.y ? 1 : 0;
-    float delta = lerp(absDeltaVector.y, absDeltaVector.x, useX) * saturate(Resolution);
-    
-    float2 increment = deltaVector / max(delta, 0.001);
-    
-    // last position where the ray missed/did not intersect any geometry
-    float search0 = 0;
-    // 0 = start fragment, 1 - end fragment
-    float search1 = 0;
-    
-    // hit during first pass
-    uint hit0 = 0;
-    // hit during second pass
-    uint hit1 = 0;
-    
-    float viewDistance = startVS.z;
-    // view distance different between the current ray point and scene position
-    // how far behind/in front of the scene the ray is
-    float depth = Thickness;
-    
-    float2 currentSS = startSS.xy;
-    uv.xy = currentSS * TexelSize;
-    
-    uint i;
-    [loop]
-    for (i = 0; i < uint(delta); ++i)
+    TraceOutput traceOutput = Trace(uv);
+    if (traceOutput.Hit)
     {
-        currentSS += increment;
-        uv = currentSS * TexelSize;
-        positionTo = SamplePositionVS(uv);
-        
-        float2 offset = (currentSS - startSS.xy) / deltaVector;
-        search1 = lerp(offset.y, offset.x, useX);
-        search1 = saturate(search1);
-        
-        viewDistance = (startVS.z * endVS.z) / lerp(endVS.z, startVS.z, search1);
-        depth = viewDistance - positionTo.z;
-        
-        if (0 < depth && depth < Thickness)
-        {
-            hit0 = 1;
-            break;
-        }
-        else
-        {
-            search0 = search1;
-        }
+        if (any(traceOutput.UV < 0 || traceOutput.UV > 1))
+            return 0;
+        return SampleSceneColor(traceOutput.UV);
     }
     
-    search1 = (search0 + search1) * 0.5;
-    uint steps = Steps * hit0;
-    
-    [loop]
-    for (i = 0; i < steps; ++i)
-    {
-        currentSS = lerp(startSS.xy, endSS.xy, search1);
-        uv = currentSS * TexelSize;
-        positionTo = SamplePositionVS(uv);
-
-        viewDistance = (startVS.z * endVS.z) / lerp(endVS.z, startVS.z, search1);
-        depth = viewDistance - positionTo.z;
-        
-        if (0 < depth && depth < Thickness)
-        {
-            hit1 = 1;
-            search1 = (search0 + search1) * 0.5;
-        }
-        else
-        {
-            float temp = search1;
-            search1 = search1 + ((search1 - search0) / 2);
-            search0 = temp;
-        }
-    }
-    
-    float visibility = hit1
-        //* (1 - max(dot(-directionFrom, reflectionDirection), 0)) // fade out if pointing towards camera
-        //* (1 - saturate(depth / Thickness)) // fade out if intersection is too far from scene geometry
-        //* (1 - saturate(length(positionTo - positionFrom) / MaxDistance)) // fade out if reflected point is too far
-        //* (any(uv < 0 || uv > 1) ? 0 : 1) // fade out if outside of viewport
-    ;
-    visibility = saturate(visibility);
-    
-    return sceneColor.Sample(defaultSampler, uv) * visibility;
+    return 0;
 }
