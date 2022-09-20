c073765552e5a7c089ccaa2c43615eaaac71e34a
Vladislav Kantaev <vvkantaev@yandex.ru>
2022-09-20 20:05:36 +0300

    Temp Remove unused code

--- DX12Renderer/Shaders/SSR_Trace_PixelShader.hlsl
+++ DX12Renderer/Shaders/SSR_Trace_PixelShader.hlsl
@@ -71,30 +71,6 @@ struct TraceOutput
     float Fade;
 };
 
-//https://virtexedge.design/shader-series-basic-screen-space-reflections/
-
-#define SAMPLES_COUNT 16
-
-static const float2 RANDOM_PIXEL_OFFSETS[SAMPLES_COUNT] =
-{
-    float2(0.94558609, -0.76890725),
-	float2(-0.81544232, -0.87912464),
-	float2(0.97484398, 0.75648379),
-	float2(-0.91588581, 0.45771432),
-	float2(-0.94201624, -0.39906216),
-	float2(-0.094184101, -0.92938870),
-	float2(0.34495938, 0.29387760),
-	float2(-0.38277543, 0.27676845),
-	float2(0.44323325, -0.97511554),
-	float2(0.53742981, -0.47373420),
-	float2(-0.26496911, -0.41893023),
-	float2(0.79197514, 0.19090188),
-	float2(-0.24188840, 0.99706507),
-	float2(-0.81409955, 0.91437590),
-	float2(0.19984126, 0.78641367),
-	float2(0.14383161, -0.14100790)
-};
-
 TraceOutput Trace(float2 uv)
 {
     TraceOutput output = (TraceOutput) 0;
