8616467969d49832b6f38bf3d4bd2a9cc543ce34
Vladislav Kantaev <vvkantaev@yandex.ru>
2022-09-27 15:05:13 +0300

    Apply formatting to CMake files

--- CMakeIncludes/CopyAssets.cmake
+++ CMakeIncludes/CopyAssets.cmake
@@ -2,4 +2,5 @@
 add_custom_command(TARGET ${TARGET_NAME} POST_BUILD
         COMMAND ${CMAKE_COMMAND} -E copy_directory
         ${CMAKE_CURRENT_SOURCE_DIR}/../Assets/
-        $<TARGET_FILE_DIR:${TARGET_NAME}>/Assets)
+        $<TARGET_FILE_DIR:${TARGET_NAME}>/Assets
+        )
--- CMakeIncludes/CopyPixDLL.cmake
+++ CMakeIncludes/CopyPixDLL.cmake
@@ -3,4 +3,4 @@ add_custom_command(TARGET ${TARGET_NAME} POST_BUILD
         COMMAND ${CMAKE_COMMAND} -E copy_if_different
         ${CMAKE_CURRENT_SOURCE_DIR}/../WinPixEventRuntime/bin/WinPixEventRuntime.dll
         $<TARGET_FILE_DIR:${TARGET_NAME}>
-)
+        )
