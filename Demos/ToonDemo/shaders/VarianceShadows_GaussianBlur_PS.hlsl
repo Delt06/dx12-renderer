d90946e608ccd13cc26f9096a6c67eed4a9d03c7
Vladislav Kantaev <vvkantaev@yandex.ru>
2022-11-04 18:20:49 +0300

    ToonDemo: reduce VSM blur kernel size Increases VSM blur pass performance > 3x

--- Demos/ToonDemo/shaders/VarianceShadows_GaussianBlur_PS.hlsl
+++ Demos/ToonDemo/shaders/VarianceShadows_GaussianBlur_PS.hlsl
@@ -1,10 +1,24 @@
 #include <ShaderLibrary/Common/RootSignature.hlsli>
 
-#include "ShaderLibrary/Gaussian.hlsli"
-
 struct PixelShaderInput
 {
     float2 UV : TEXCOORD;
+};
+
+const static uint BlurKernelSize = 5;
+
+const static float BlurOffsets[BlurKernelSize] =
+{
+    -3.2307692308f, -1.3846153846f,
+    0.0f,
+    1.3846153846f, 3.2307692308f
+};
+
+const static float BlurWeights[BlurKernelSize] =
+{
+    0.0702702703f, 0.3162162162f,
+    0.2270270270f,
+    0.3162162162f, 0.0702702703f
 };
 
 cbuffer Parameters : register(b0)
@@ -19,10 +33,10 @@ float2 main(PixelShaderInput IN) : SV_TARGET
 {
     float2 value = 0;
 
-    for (int i = 0; i < GaussianFilterSize; ++i)
+    for (uint i = 0; i < BlurKernelSize; ++i)
     {
-        float2 uv = IN.UV + direction * (i - GaussianIndexOffset) * texelSize;
-        value += sourceTexture.Sample(g_Common_PointClampSampler, uv) * GaussianCoefficients[i];
+        float2 uv = IN.UV + direction * BlurOffsets[i] * texelSize;
+        value += sourceTexture.Sample(g_Common_PointClampSampler, uv) * BlurWeights[i];
     }
 
     return value;
